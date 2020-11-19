94b7cef921156cf088d728abcbf0e5f71823a7f6
Radim Vansa <rvansa@redhat.com>
2020-11-19 16:40:44 +0100

    Fix scaling

--- rescale.sh
+++ rescale.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 CONTROL_PLANE=$1
-GATEWAYS=$2
+ROUTERS=$2
+GATEWAYS=$3
 
+oc scale ingresscontrollers.operator.openshift.io default -n openshift-ingress-operator --replicas=$ROUTERS
 oc scale -n $CONTROL_PLANE deployment istio-ingressgateway --replicas=$GATEWAYS
+
 for attempt in $(seq 1 60); do
+    CURR_RT=$(oc get po -n openshift-ingress -o json | jq '.items | length')
     CURR_GW=$(oc get po -n $CONTROL_PLANE -l app=istio-ingressgateway -o json | jq '.items | length')
-    if [ "$CURR_GW" = "$GATEWAYS" ]; then
-        echo "Mesh has $CURR_GW gateways."
+    echo "Mesh has $CURR_RT routers and $CURR_GW gateways."
+    if [ "$CURR_GW" = "$GATEWAYS" -a "$CURR_RT" = "$ROUTERS" ]; then
         break;
     fi;
     echo "Attempt $attempt/60"
